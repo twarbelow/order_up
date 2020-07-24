542e1298f69d39721817dbd91254803181501a28
twarbelow <taija.warbelow@google.com>
2020-07-24 08:50:49 -0700

    model spec dish test calories

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,16 @@ RSpec.describe Dish, type: :model do
     it {should have_many :dish_ingredients}
     it {should have_many(:ingredients).through(:dish_ingredients)}
   end
+
+  it 'can sum calories' do
+    jordan = Chef.create(name: "Jordan Feldstein")
+    yum = Dish.create(name: "PBJ", description: "peanut butter jelly time", chef_id: "#{jordan.id}")
+    peanut_butter = yum.ingredients.create(name: "peanut butter", calories: 150)
+    jelly = yum.ingredients.create(name: "raspberry jelly", calories: 80)
+    bread = yum.ingredients.create(name: "bread", calories: 120)
+
+    total = yum.calories
+
+    expect(total).to eq(350)
+  end
 end
