e493a623caec4a9279d96edbd4fa4d9827ed0a3b
twarbelow <taija.warbelow@google.com>
2020-07-24 09:35:32 -0700

    ingredients action for chef

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -2,4 +2,8 @@ class ChefsController < ApplicationController
   def show
     @chef = Chef.find(params[:id])
   end
+
+  def ingredients
+    @ingredients = Chef.find(params[:id]).ingredients.uniq
+  end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :ingredients, through: :dishes
 
 end
